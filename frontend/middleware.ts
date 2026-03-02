d4c902ac3c139243094377e944d9bf9ef293962c
Aryan Gawade <147228189+NoB0T21@users.noreply.github.com>
2026-03-02 20:13:27 +0530

    update middleware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -6,29 +6,25 @@ export async function middleware (request: NextRequest){
     const isPublicRout = path === '/sign-up' || path === '/sign-in'
     let token = request.cookies.get('token')?.value;
 
-    const user = await api.get('/user/valid',{
-        headers:{
-            Authorization:`Bearer ${token}`,
-        },
-        withCredentials:true
-    })
-
-    const verified:boolean = user.data.success
-    if(!verified){
-        request.cookies.set('token', '')
-    }
-
-    token = request.cookies.get('token')?.value;
-    if(!token && !isPublicRout){
-        const res = NextResponse.redirect(new URL('/sign-up',request.url))
-        res.headers.set('x-middleware-cache', 'no-cache');
-        return res
-    }
-    
-    if(token && isPublicRout && verified ){
-        const res = NextResponse.redirect(new URL('/',request.url))
-        res.headers.set('x-middleware-cache', 'no-cache');
+    try {
+        const user = await api.get('/user/valid',{
+            headers:{
+                Authorization:`Bearer ${token}`,
+            },
+            
+            withCredentials:true
+        })
+        const verified:boolean = user.data.success
+        if(token && isPublicRout && verified ){
+            const res = NextResponse.redirect(new URL('/',request.url))
+            res.headers.set('x-middleware-cache', 'no-cache');
+            return res
+        }
         
+    } catch (error) {
+        request.cookies.set('token', '')
+        const res = NextResponse.redirect(new URL('/sign-in',request.url))
+        res.headers.set('x-middleware-cache', 'no-cache');
         return res
     }
 
