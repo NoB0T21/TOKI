66e0a88469551addc605eb754fbd4aa01f05a201
NoB0T21 <147228189+NoB0T21@users.noreply.github.com>
2026-03-15 19:42:39 +0530

    fix

--- frontend/context/tankstack.tsx
+++ frontend/context/tankstack.tsx
@@ -1,19 +1,12 @@
-'use client'
+'use client';
 
 import { Provider } from "react-redux";
 import { store } from "@/store/store";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const [mounted, setMounted] = useState(false);
   const [queryClient] = useState(() => new QueryClient());
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) return <>{children}</>;
 
   return (
     <Provider store={store}>
